88b4da980e5b02b05bf362b1ca98fd0cfb7b0c89
diruocco-aniellomarco <aniello.marco.dr@gmail.com>
2023-07-20 19:37:10 +0200

    add message allert "Campo obbligatorio"

--- js/main.js
+++ js/main.js
@@ -11,37 +11,56 @@ const userName = document.getElementById('user_name');
 const cancelButton = document.getElementById('cancel_information');
 
 sendButton.addEventListener('click', function() {
-    // calcolo costo biglietto
-    let ticketCost = kmUser.value * 0.21;
-    // Sconto base per maggiorenni sotto i 65 anni
-    let discount = 'Nessuno sconto applicato per questa fascia di età';
-    let tipeTicket= 'Biglietto Standard'
 
-    const carriageRandom = Math.floor(Math.random()* 15) + 1;
+    // Per capire se sono stati inseriti nome utente e Km che deve percorrere
+    if (userName.value != '' && kmUser.value != ''){
+        
+        // calcolo costo biglietto
+        let ticketCost = kmUser.value * 0.21;
+        // Sconto base per maggiorenni sotto i 65 anni
+        let discount = 'Nessuno sconto applicato per questa fascia di età';
+        let tipeTicket= 'Biglietto Standard'
 
-    const codiceCP = Math.floor(Math.random()* 99999) + 1;
+        const carriageRandom = Math.floor(Math.random()* 15) + 1;
 
-    if (userAge.value == '2') {
-        ticketCost = ticketCost - ((ticketCost / 100) * 20);
-        discount = 'lo sconto per questa fascia d\'età è del 20%';
-        tipeTicket= 'Sconto minorenni'
-    }
-    if (userAge.value == '3') {
-        ticketCost = ticketCost - ((ticketCost / 100) * 40);
-        discount = 'lo sconto per questa fascia d\'età è del 40%';
-        tipeTicket= 'Sconto over 65'
-    }
-    // visualizzo il biglietto
-    document.getElementById('ticket').classList.remove('d-none');
+        const codiceCP = Math.floor(Math.random()* 99999) + 1;
+
+        if (userAge.value == '2') {
+            ticketCost = ticketCost - ((ticketCost / 100) * 20);
+            discount = 'lo sconto per questa fascia d\'età è del 20%';
+            tipeTicket= 'Sconto minorenni'
+        }
+        if (userAge.value == '3') {
+            ticketCost = ticketCost - ((ticketCost / 100) * 40);
+            discount = 'lo sconto per questa fascia d\'età è del 40%';
+            tipeTicket= 'Sconto over 65'
+        }
+        // visualizzo il biglietto
+        document.getElementById('ticket').classList.remove('d-none');
+
+        // nel caso fossero comparsi, tolgo i messaggi di allerta
+        document.getElementById('allert_name').classList.add('d-none');
+        document.getElementById('allert_km').classList.add('d-none');
+
+        document.getElementById('ticketCost').innerHTML = ticketCost.toFixed(2) + '€';
+        document.getElementById('cost_details').innerHTML = ticketCost.toFixed(2) + '€';
+        document.getElementById('discount').innerHTML = discount;
+        document.getElementById('name_ticket').innerHTML = userName.value;
+        document.getElementById('tipe_ticket').innerHTML = tipeTicket;
+        document.getElementById('carriage').innerHTML = carriageRandom;
+        document.getElementById('codice_cp').innerHTML = codiceCP;
     
-
-    document.getElementById('ticketCost').innerHTML = ticketCost.toFixed(2) + '€';
-    document.getElementById('cost_details').innerHTML = ticketCost.toFixed(2) + '€';
-    document.getElementById('discount').innerHTML = discount;
-    document.getElementById('name_ticket').innerHTML = userName.value;
-    document.getElementById('tipe_ticket').innerHTML = tipeTicket;
-    document.getElementById('carriage').innerHTML = carriageRandom;
-    document.getElementById('codice_cp').innerHTML = codiceCP;
+    } else if (userName.value == '' && kmUser.value == '') {
+        document.getElementById('allert_name').classList.remove('d-none');
+        document.getElementById('allert_km').classList.remove('d-none');
+        } else {
+            if (userName.value == '') {
+                document.getElementById('allert_name').classList.remove('d-none');
+            } else {
+                document.getElementById('allert_km').classList.remove('d-none');
+            }
+        }
+    
 });
 
 cancelButton.addEventListener('click', function() {
@@ -51,27 +70,3 @@ cancelButton.addEventListener('click', function() {
     userName.value = '';
 });
 
-
-
-// let ticketCost = km * 0.21;
-// // console.log(ticketCost);
-// let discount = 'Nessuno sconto applicato per questa fascia di età';
-
-// if (userAge < 18){
-    // ticketCost = ticketCost - ((ticketCost / 100) * 20);
-    // discount = 'lo sconto per questa fascia d\'età è del 20%';
-// }
-
-// if (userAge > 65){
-//     ticketCost = ticketCost - ((ticketCost / 100) * 40);
-//     discount = 'lo sconto per questa fascia d\'età è del 40%';
-// }
-
-// // console.log(ticketCost);
-
-// document.getElementById('km').innerHTML = km;
-// document.getElementById('userAge').innerHTML = userAge;
-
-// document.getElementById('ticketCost').innerHTML = ticketCost.toFixed(2) + '€';
-
-// document.getElementById('discount').innerHTML = discount;
