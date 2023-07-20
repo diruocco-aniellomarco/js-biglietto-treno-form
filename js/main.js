ad09f6ca451ac94fc33fb6bb0bfe66b0708e04b7
diruocco-aniellomarco <aniello.marco.dr@gmail.com>
2023-07-20 16:46:16 +0200

    Add: MILESTONE 1 - FIX readme

--- js/main.js
+++ js/main.js
@@ -1,25 +1,54 @@
-// const km = parseFloat(prompt('Quanti km vuoi percorrere?'));
-// const userAge= parseInt(prompt('Quanti anni hai?'));
 
-let ticketCost = km * 0.21;
-// console.log(ticketCost);
-let discount = 'Nessuno sconto applicato per questa fascia di età';
+// quanti km da percorrere
+const kmUser = document.getElementById('km_user');
+// quanti anni
+const userAge = document.getElementById('user_age');
+// Bottone invio dati
+const sendButton = document.getElementById('send_information');
 
-if (userAge < 18){
-    ticketCost = ticketCost - ((ticketCost / 100) * 20);
-    discount = 'lo sconto per questa fascia d\'età è del 20%';
-}
 
-if (userAge > 65){
-    ticketCost = ticketCost - ((ticketCost / 100) * 40);
-    discount = 'lo sconto per questa fascia d\'età è del 40%';
-}
+sendButton.addEventListener('click', function() {
+    // calcolo costo biglietto
+    let ticketCost = kmUser.value * 0.21;
+    // Sconto base per maggiorenni sotto i 65 anni
+    let discount = 'Nessuno sconto applicato per questa fascia di età';
 
-// console.log(ticketCost);
+    if (userAge.value == '2') {
+        ticketCost = ticketCost - ((ticketCost / 100) * 20);
+        discount = 'lo sconto per questa fascia d\'età è del 20%';
+    }
+    if (userAge.value == '3') {
+        ticketCost = ticketCost - ((ticketCost / 100) * 40);
+        discount = 'lo sconto per questa fascia d\'età è del 40%';
+    }
+    document.getElementById('ticketCost').innerHTML = ticketCost.toFixed(2) + '€';
+    document.getElementById('discount').innerHTML = discount;
+    
+});
 
-document.getElementById('km').innerHTML = km;
-document.getElementById('userAge').innerHTML = userAge;
 
-document.getElementById('ticketCost').innerHTML = ticketCost.toFixed(2) + '€';
 
-document.getElementById('discount').innerHTML = discount;
+
+
+// let ticketCost = km * 0.21;
+// // console.log(ticketCost);
+// let discount = 'Nessuno sconto applicato per questa fascia di età';
+
+// if (userAge < 18){
+    // ticketCost = ticketCost - ((ticketCost / 100) * 20);
+    // discount = 'lo sconto per questa fascia d\'età è del 20%';
+// }
+
+// if (userAge > 65){
+//     ticketCost = ticketCost - ((ticketCost / 100) * 40);
+//     discount = 'lo sconto per questa fascia d\'età è del 40%';
+// }
+
+// // console.log(ticketCost);
+
+// document.getElementById('km').innerHTML = km;
+// document.getElementById('userAge').innerHTML = userAge;
+
+// document.getElementById('ticketCost').innerHTML = ticketCost.toFixed(2) + '€';
+
+// document.getElementById('discount').innerHTML = discount;
--- readme.md
+++ readme.md
@@ -9,7 +9,11 @@ Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio,
 - va applicato uno sconto del 20% per i minorenni
 - va applicato uno sconto del 40% per gli over 65.
 
-L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
+ - MILESTONE 1:
+    - Iniziamo implementando il programma senza alcuna estetica: usando esclusivamente due input e un bottone (non stilizzati), realizziamo le specifiche scritte sopra. La risposta finale (o output) sarà anch'essa da scrivere in console.
+- MILESTONE 2:
+    - Solo una volta che il milestone 1 sarà completo e funzionante allora realizzeremo un form in pagina in cui l'utente potrà inserire i dati e visualizzare il calcolo finale con il prezzo.
+    Il recap dei dati e l'output del prezzo finale, andranno quindi stampati in pagina (il prezzo dovrà essere formattato con massimo due decimali, per indicare i centesimi sul prezzo). Questo richiederà un minimo di ricerca.
 
 # Svolgimento
  (Userò questa in quanto più semplice e diretta)
